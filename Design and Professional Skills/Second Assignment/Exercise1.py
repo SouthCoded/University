063544e9b3cfb8ef280d953eec7d1f8366f57ea5
Duncan-Donut <duncan.d.rowe@gmail.com>
2017-12-10 21:23:51 +0000

    Added Exercise1

---
+++ Design and Professional Skills/Second Assignment/Exercise1.py
@@ -0,0 +1,41 @@
+#Exercise 1 
+
+#        (w,v)                   (mx w)
+#thief([(2,50), (3,100), (5,140)], 17) = 550
+
+def thief(obj,max_weight):
+
+	try:
+		#Creates array of length max_weight and fills with 0
+		weightArray = [max_weight+1]
+		weightArray = [0 for i in range(max_weight+1)]
+
+		obj.sort()
+
+		#Goes through the list
+		for w in range(max_weight):
+			#Goes through the object
+			for i in range(len(obj)):
+				#If the object weight is less than the weight 			
+				if obj[i][0] <= w+1:
+					# Checks weight and seems if for that weight that is the maximum value
+					# or if the object added increases its value
+					weightArray[w+1] = max(weightArray[w+1 - obj[i][0]] + obj[i][1],weightArray[w+1])
+
+		return weightArray[max_weight]
+	except TypeError:
+		return "Wrong Type"
+
+
+def test_thief_example():
+	assert thief([(2,50), (3,100), (5,140)], 17) == 550
+
+def test_thief_zero():
+	assert thief([(2,50), (3,100), (5,140)], 17) > 0
+
+		
+
+
+
+
+
