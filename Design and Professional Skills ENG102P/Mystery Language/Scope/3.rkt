e17a2cd7a09cf4c5b0ff94e62280210e9789d644
South <duncan.pf2@gmail.com>
2018-02-09 14:06:14 +0000

    Added ML 3 and 4

---
+++ Design and Professional Skills ENG102P/Mystery Language/Scope/3.rkt
@@ -0,0 +1,12 @@
+#lang Scope
+
+
+fun test(var2):
+
+var2 = var2 + 2
+var2
+
+end
+
+test(3)
+
---
+++ Design and Professional Skills ENG102P/Mystery Language/Scope/4.rkt
@@ -0,0 +1,16 @@
+#lang Scope
+
+find = 3
+
+
+fun test():
+
+   var := find + 3
+   var
+end
+
+
+test()
+
+
+
